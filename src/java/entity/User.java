bdd629368ff6231d108e86d8f16c38cad92147f3
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-11-15 10:33:53 +0530

    need to optimize

--- src/java/entity/User.java
+++ src/java/entity/User.java
@@ -4,10 +4,31 @@
  */
 package entity;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author rc007
  */
-public class User {
-    
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @Column(name = "email", unique = true, length = 100, nullable = false)
+    private String email;
+    @Column(name = "password", length = 45, nullable = false)
+    private String password;
+    @Column(name = "name", length = 45, nullable = false)
+    private String name;
+    @Column(name = "date_joined", nullable = false)
+    private Date date_joined;
+    @Column(name = "status", length = 45, nullable = false)
+    private String status;
+
 }
